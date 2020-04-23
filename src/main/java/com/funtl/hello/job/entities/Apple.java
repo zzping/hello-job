75ba9b6e9bf2ae349b194ad75a6fca12c3c1c4ea
zzp <zzp@example.com>
2020-04-23 17:08:58 +0800

    t

--- src/main/java/com/funtl/hello/job/entities/Apple.java
+++ src/main/java/com/funtl/hello/job/entities/Apple.java
@@ -2,8 +2,10 @@ package com.funtl.hello.job.entities;
 
 public class Apple {
 
+
     private String name = "Apple - 1";
 
+    private String id = "1";
     public String getName() {
         return name;
     }
